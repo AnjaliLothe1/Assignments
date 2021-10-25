99ae17d9280232074e4ba26de90d2dde5bae70c0
AnjaliLothe1 <91319580+AnjaliLothe1@users.noreply.github.com>
2021-10-26 03:11:46 +0530

    Update Palindrome.java

--- Assignment2/Palindrome.java
+++ Assignment2/Palindrome.java
@@ -3,17 +3,12 @@ package Assignment2;
 public class Palindrome {
 	 
     public static void main(String[] args) {
-         
-        int n = 122321;
-         
-        //reverse the string
-        int rev = Integer.parseInt((new StringBuilder(n+"")).reverse().toString());
-         
-        //check if n is palindrome
-        if(n==rev) {
-            System.out.println(n+" is Palindrome.");
+        int num = 121
+        int rev = Integer.parseInt((new StringBuilder(num+"")).reverse().toString());
+        if(num==rev) {
+            System.out.println(num+" = Palindrome");
         } else {
-            System.out.println(n+" is not Palindrome.");
+            System.out.println(num+" = not Palindrome");
         }
     }
 }
