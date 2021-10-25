7c3be28dc9b850cfc004cbdd6a3f4da630e1b1cb
AnjaliLothe1 <91319580+AnjaliLothe1@users.noreply.github.com>
2021-10-26 03:18:41 +0530

    Update Indices.java

--- Assignment2/Indices.java
+++ Assignment2/Indices.java
@@ -2,43 +2,40 @@ package Assignment2;
 
 import java.util.*;
 public class Indices {
-
-
-    // Time complexity: O(n^2)
-    private static int[] Indices(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
+    private static int[] Indices(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.length; i++) 
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
                     return new int[] { i, j };
                 }
             }
         }
         return new int[] {};
     }
-
-
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter length of array : ");
-        int n = keyboard.nextInt();
+        int n = sc.nextInt();
         int[] nums = new int[n];
         
         System.out.println("Enter the element of array : ");
-        for(int i = 0; i < n; i++) {
-            nums[i] = keyboard.nextInt();
+        for(int i = 0; i < n; i++) 
+        {
+            nums[i] = sc.nextInt();
         }
-        
         System.out.println("Enter Target value which we want : ");
-        int target = keyboard.nextInt();
-
-        keyboard.close();
-
+        int target = sc.nextInt();
         int[] indices = Indices(nums, target);
 
         if (indices.length == 2) {
             System.out.println(indices[0] + " " + indices[1]);
         } else {
-            System.out.println("No solution found!");
+            System.out.println("Target not matched...Try again....!  ");
         }
     }
 }
